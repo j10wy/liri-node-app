e9a1795e570d01a84c31aff3be1279f6973d3ff8
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-18 11:15:03 -0700

    Reponse for 0 tracks
    Added a response when 0 tracks are returned from the spotify search.

--- commands/spotify.js
+++ commands/spotify.js
@@ -28,20 +28,26 @@ async function getTracks(song_title) {
 function processTracks(response) {
 
 	var tracksResponseArray = response.tracks.items;
+	var numTracks = tracksResponseArray.length;
 
-	tracksResponseArray.map(trackInfo => {
-		var artist_name = propercase(trackInfo.artists[0].name);
-		var song_title = trackInfo.name;
-		var album_title = trackInfo.album.name;
-		var preview_link = trackInfo.preview_url ? trackInfo.preview_url : "Preview not available".error;
+	if (numTracks > 0) {
+		tracksResponseArray.map(trackInfo => {
+			var artist_name = propercase(trackInfo.artists[0].name);
+			var song_title = trackInfo.name;
+			var album_title = trackInfo.album.name;
+			var preview_link = trackInfo.preview_url ? trackInfo.preview_url : "Preview not available".error;
 
-		console.log(`\nArtist:`.spotify,`${artist_name}`.white);
-		console.log(`Song:`.spotify, `${song_title}`.white);
-		console.log(`Album:`.spotify,`${album_title}`.white);
-		console.log(`URL:`,`${preview_link}`.spotify.underline);
-		console.log("\n--------------------------------------\n");
+			console.log(`\nArtist:`.spotify, `${artist_name}`.white);
+			console.log(`Song:`.spotify, `${song_title}`.white);
+			console.log(`Album:`.spotify, `${album_title}`.white);
+			console.log(`URL:`, `${preview_link}`.spotify.underline);
+			console.log("\n--------------------------------------\n");
 
-	});
+		});
+	} else {
+		console.log("Could not find any tracks with this title.".error);
+	}
+
 }
 
 module.exports = getTracks;
