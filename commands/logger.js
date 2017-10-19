ac006478c6a7c4b4a9cf68109b6a250e7bdc5656
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-19 14:10:18 -0700

    Changed history to a .log file

--- commands/logger.js
+++ commands/logger.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function log_data(textToLog) {
-	fs.appendFile("./log_history.txt", textToLog, (error, data) => {
+	fs.appendFile("./history.log", textToLog, (error, data) => {
 		if (error) throw Error("Something went wrong while logging data.")
 	});
 }
@@ -10,7 +10,7 @@ function log_command(arguments) {
 	let command = arguments[2];
 	let value = arguments[3];
 	let logCommandText = `\n*** Command: ${command} | Value: ${value} ***\n`
-	fs.appendFile("./log_history.txt", logCommandText, (error, data) => {
+	fs.appendFile("./history.log", logCommandText, (error, data) => {
 		if (error) throw Error("Something went wrong while logging history.")
 	});
 }
